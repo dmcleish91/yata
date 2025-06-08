2043ce373f15449663eecc062d049e09a360b297
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2025-06-08 01:57:39 -0400

    refactor: update route for auth callback to use lowercase filename for consistency

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,5 @@ export default [
   layout('components/RequireAuth.tsx', [route('/', 'routes/Home.tsx')]),
   route('about', 'routes/About.tsx'),
   route('login', 'routes/Login.tsx'),
-  route('auth/callback', 'routes/AuthCallBack.tsx'),
+  route('auth/callback', 'routes/authcallback.tsx'),
 ] satisfies RouteConfig;
